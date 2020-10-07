a743290ff3e0c2364a28bcfb433f985f2dc78d79
AlexSklizovic <sklizo44@gmail.com>
2020-10-07 13:12:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# workspace-javascript-1tagya
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/workspace-javascript-1tagya)
---
+++ js/hello.js
@@ -0,0 +1,18 @@
+// Send a customized message back based on time
+
+console.log("I'm in the hello.js file");
+
+// Get date and time
+let today = new Date();
+let hoursNow = today.getHours();
+
+// Display greeting message
+let greetingMsg = "";
+if (hoursNow < 12){
+  greetingMsg = "Good morning!";
+} else {
+  greetingMsg = "Good evening!"
+}
+
+// Write to the document
+document.write(greetingMsg);
