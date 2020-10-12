ffc86b019180566fca714ac9e5af5c03dac39204
AlexSklizovic <sklizo44@gmail.com>
2020-10-12 13:13:36 -0400

    1 file modified, 1 added

---
+++ js/arith.js
@@ -0,0 +1,11 @@
+// Get element
+
+// Element --- property
+let num1 = parseInt(document.getElementById("n1").textContent); 
+let num2 = parseInt(document.getElementById("n2").textContent); 
+
+// Process
+let c = num1 + num2;
+
+// Display
+document.write(c);
